5e48308f0ed56d0c27a0bf2e51f0e504ef0d7fe4
Petri Konkka <petrikonkka@gmail.com>
2017-02-11 09:31:04 +0200

    Number of exercises shown on the workout list.

--- src/app/workouts/workout-list/workout-item.component.ts
+++ src/app/workouts/workout-list/workout-item.component.ts
@@ -1,16 +1,25 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 import { Workout } from '../../shared/model/workout';
 import * as moment from 'moment';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-workout-item',
   templateUrl: './workout-item.component.html',
   styleUrls: ['./workout-item.component.css']
 })
-export class WorkoutItemComponent {
+export class WorkoutItemComponent implements OnInit {
   @Input() workout: Workout;
   @Input() workoutId: number;
+
+  exerciseCount: Number;
+
+  ngOnInit() {
+
+    this.exerciseCount = _.values(this.workout.exercises).length;
+
+  }
 
   // -----------------------------------------------------------
   toDate() {
