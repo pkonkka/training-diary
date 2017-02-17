4acfb0057aa0c788b382029ae2b186428d15c432
Petri Konkka <petrikonkka@gmail.com>
2017-02-17 11:30:16 +0200

    #46: Exercise item fixed

--- src/app/exercises/exercise-list/exercise-item.component.ts
+++ src/app/exercises/exercise-list/exercise-item.component.ts
@@ -1,22 +1,37 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs/Rx';
 
+import { Category } from '../../shared/model/category';
 import { Exercise } from '../../shared/model/exercise';
+import { ExerciseService } from '../../shared/model/exercise.service';
 
 @Component({
   selector: 'app-exercise-item',
   templateUrl: './exercise-item.component.html',
   styleUrls: ['./exercise-item.component.css']
 })
-export class ExerciseItemComponent implements OnInit {
+export class ExerciseItemComponent implements OnInit, OnDestroy {
   @Input() exercise: Exercise;
   @Input() exerciseId: number;
 
-  constructor() { 
+  private exerciseSub: Subscription;
+  private relatedCategpries: Category[];
 
+  constructor(private exerciseService: ExerciseService) { 
 
   }
 
   ngOnInit() {
+  
+    this.exerciseSub = this.exerciseService.findAllCategoriesForExercise(this.exercise.url)
+      .subscribe(
+        categories => this.relatedCategpries = categories
+      );
+
+  }
+
+  ngOnDestroy() {
+    this.exerciseSub.unsubscribe();
   }
 
 }
